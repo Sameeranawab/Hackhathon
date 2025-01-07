dc4f80c2ded1a2e3c5070bde7cf125381a2c4bb0
Sameera Nawab <160266378+Sameeranawab@users.noreply.github.com>
2025-01-07 18:51:22 +0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,1 @@
-type ClassValue = string | string[] | undefined;
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-function clsx(inputs: ClassValue[]): any {
-    throw new Error("Function not implemented.");
-}
-function twMerge(arg0: any) {
-    throw new Error("Function not implemented.");
-}
 
